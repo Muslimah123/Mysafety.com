cc13b9113ac9d7bfd9e1c11f9247a4530f613ea5
IDAYA SEIDU <idaya.seidu@ashesi.edu.gh>
2022-12-07 20:21:47 +0000

    cleardb config

--- online-shopping-system/db.php
+++ online-shopping-system/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$db = "onlineshop";
+$servername = "us-cdbr-east-06.cleardb.net";
+$username = "b15aea1f32da82";
+$password = "9f2a3aae";
+$db = "heroku_8a6e96110881af4";
 
 // Create connection
 $con = mysqli_connect($servername, $username, $password,$db);
